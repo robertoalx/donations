77b462fc0c96c2cc307da07f1dd888ba3e35f7ca
Alexandru Robert <romaniarobertverde@yahoo.com>
2018-10-24 21:21:55 +0100

    We got DATA WOHOO

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
+import DEV_ID from './constant';
+
 
 class App extends Component {
   state = {
+    charity: null,
+    donationList: [],
     charityList: [
       {
         charityName: "British Heart Foundation",
@@ -32,14 +36,26 @@ class App extends Component {
     ]
   }
 
-  renderCharity = charityId => {
-    console.log(charityId)
+  getData = charityId => {
+    const CHARITY_URL = `https://api.justgiving.com/${DEV_ID}/v1/charity/${charityId}`
+    const CHARITY_DONATION_URL = `https://api.justgiving.com/${DEV_ID}/v1/charity/${charityId}/donations`
+    // console.log(CHARITY_URL, CHARITY_DONATION_URL)
+    axios.all(
+      [
+        axios.get(CHARITY_URL), 
+        axios.get(CHARITY_DONATION_URL)
+      ]
+    ).then(axios.spread(({data: charity}, {data: {donations: donationList}}) => {
+      console.log(charity, donationList)
+    }))
   }
 
   renderCharityList = () => {
-    return this.state.charityList.map(v => {
-        return <div key={v.id}
-                    onClick={() => this.renderCharity(v.id)}>{v.charityName}</div>
+    return this.state.charityList.map(iAmAwesomeee => {
+        return  <div key={iAmAwesomeee.id}
+                    onClick={() => this.getData(iAmAwesomeee.id)}>
+                    == {iAmAwesomeee.charityName} ==
+                </div>
       }
     )
   }
@@ -48,6 +64,8 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Drop comments on Facebook!!!</h1> 
+        <div>donation: {JSON.stringify(this.state.charity)}</div>
+        <div>donation: {JSON.stringify(this.state.donationList)}</div>
         {this.renderCharityList()}
       </div>
     );
---
+++ src/constant.js
@@ -0,0 +1,2 @@
+const DEV_ID = '1da94ae8';
+export default DEV_ID
